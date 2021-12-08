72425b31f9982e8f8da1e6d4ccf66437a9425dd8
Arseny Kurin <58228813+arsikurin@users.noreply.github.com>
2021-12-08 11:20:40 +0300

    7.12
    done tutorial
    add how-to-install

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+\
+,.idea/inspectionProfiles/Project_Default.xml,4/9/496a238a6afa168dbaf6efd37bb459331589579c
+^
+..idea/inspectionProfiles/profiles_settings.xml,1/e/1e9075f5bf079c01ef2c910709e91a497d262080
+7
+Pipfile,1/5/15af4b3d30054104e85f65a444b1db188c5deefb
+F
+dependencies/vector.py,4/4/44a25eb417db7093d78e46debbb76b620fca2293
+H
+dependencies/__init__.py,c/3/c3fbacf7fe74cc55b359f53316568f4eabc26287
+<
+Pipfile.lock,e/5/e563262209b7c2e62d1869582c8134583baa2204
+7
+PLAN.md,a/b/ab32b47ca59bfb6aa7f9a754f3ab9757c70b7dc3
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import collections
+import math
+import sys
 
 import cv2
 import mediapipe as mp
@@ -11,53 +13,202 @@ Results = collections.namedtuple("Results", field_names=("multi_hand_landmarks",
 def get_points(landmark, shape):
     points = []
     for mark in landmark:
-        points.append([mark.x * shape[1], mark.y * shape[0]])
+        points.append((mark.x * shape[1], mark.y * shape[0]))
     return np.array(points, dtype=np.int32)
 
 
 def palm_size(landmark, shape):
+    """
+    :return: palm enclosing circle radius
+    """
+
     x1, y1 = landmark[0].x * shape[1], landmark[0].y * shape[0]
     x2, y2 = landmark[5].x * shape[1], landmark[5].y * shape[0]
-    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** .5
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 
-with mp.solutions.hands.Hands(
-        static_image_mode=True,
-        max_num_hands=1,
-        min_detection_confidence=0.5
-) as handsDetector:
-    cap = cv2.VideoCapture(0)
-    prev_fist = False
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if cv2.waitKey(1) & 0xFF == ord("q") or not ret:
-            break
-        flipped = np.fliplr(frame)
-        flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
-        results: Results = handsDetector.process(flippedRGB)
+def help_():
+    with mp.solutions.hands.Hands(
+            static_image_mode=True,
+            max_num_hands=1,
+            min_detection_confidence=.2
+    ) as handsDetector:
+        flags = [0, 0, 0]
+        cap = cv2.VideoCapture(0)
+        prev_fist = False
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if cv2.waitKey(1) & 0xFF == ord("q") or not ret:
+                break
+            flipped = np.fliplr(frame)
+            flippedRGB = cv2.cvtColor(flipped, code=cv2.COLOR_BGR2RGB)
+            results: Results = handsDetector.process(flippedRGB)
+            if results.multi_hand_landmarks is not None:
+                cv2.circle(
+                    flippedRGB,
+                    center=(int(results.multi_hand_landmarks[0].landmark[4].x * flippedRGB.shape[1]),
+                            int(results.multi_hand_landmarks[0].landmark[4].y * flippedRGB.shape[0])),
+                    radius=5, color=(255, 0, 0), thickness=-1
+                )
+                cv2.circle(
+                    flippedRGB, center=(int(results.multi_hand_landmarks[0].landmark[20].x * flippedRGB.shape[1]),
+                                        int(results.multi_hand_landmarks[0].landmark[20].y * flippedRGB.shape[0])),
+                    radius=5, color=(255, 0, 0), thickness=-1
+                )
+                cv2.line(
+                    flippedRGB,
+                    pt1=(int(results.multi_hand_landmarks[0].landmark[4].x * flippedRGB.shape[1]),
+                         int(results.multi_hand_landmarks[0].landmark[4].y * flippedRGB.shape[0])),
+                    pt2=(int(results.multi_hand_landmarks[0].landmark[20].x * flippedRGB.shape[1]),
+                         int(results.multi_hand_landmarks[0].landmark[20].y * flippedRGB.shape[0])),
+                    color=(255, 0, 0), thickness=2
+                )
 
-        if results.multi_hand_landmarks is not None:
-            cv2.drawContours(
-                flippedRGB, (get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape),),
-                0, (255, 0, 0), 2
-            )
-            (x, y), r = cv2.minEnclosingCircle(get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape))
-            ws = palm_size(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
-            if 2 * r / ws > 1.5:
-                cv2.circle(flippedRGB, (int(x), int(y)), int(r), (0, 0, 255), 2)
-                if prev_fist:
-                    sbc.fade_brightness(finish=100, start=1, blocking=False, increment=10)
-                    prev_fist = False
+                (x, y), r = cv2.minEnclosingCircle(
+                    get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape))
+                ws = palm_size(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
+
+                if flags[0] < 7:
+                    cv2.putText(
+                        flippedRGB, text=f"Try clenching your fist ({flags[0]} / 6)",
+                        org=(10, flippedRGB.shape[0] - 10),
+                        fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1, color=(255, 255, 255), thickness=1
+                    )
+                    if 2 * r / ws > 1.6:
+                        cv2.circle(flippedRGB, center=(int(x), int(y)), radius=int(r), color=(0, 0, 255), thickness=2)
+                        if prev_fist:
+                            prev_fist = False
+                            flags[0] += 1
+                        cv2.putText(
+                            flippedRGB, text="status: [PALM]", org=(10, 50), fontFace=cv2.FONT_HERSHEY_PLAIN,
+                            fontScale=3, color=(255, 255, 255), thickness=2
+                        )
+                    else:
+                        cv2.circle(flippedRGB, (int(x), int(y)), int(r), (0, 255, 0), 2)
+                        if not prev_fist:
+                            prev_fist = True
+                            flags[0] += 1
+                        cv2.putText(
+                            flippedRGB, text="status: [FIST]", org=(10, 50), fontFace=cv2.FONT_HERSHEY_PLAIN,
+                            fontScale=3, color=(255, 255, 255), thickness=2
+                        )
+
+                elif flags[1] < 7:
+                    cv2.putText(
+                        flippedRGB, text="Try rotating your unclenched fist", org=(10, flippedRGB.shape[0] - 40),
+                        fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1, color=(255, 255, 255), thickness=1
+                    )
+                    cv2.putText(
+                        flippedRGB, text=f"around axis of the ordinate ({flags[1]} / 6)", org=(10, flippedRGB.shape[0] - 10),
+                        fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1, color=(255, 255, 255), thickness=1
+                    )
+                    if 2 * r / ws > 1.6:
+                        cv2.circle(flippedRGB, center=(int(x), int(y)), radius=int(r), color=(0, 0, 255), thickness=2)
+                        pinky_tip = results.multi_hand_landmarks[0].landmark[20]
+                        thumb_tip = results.multi_hand_landmarks[0].landmark[4]
+                        if (pinky_tip.x - thumb_tip.x + .2) * 200 > 60 and flags[2] == 0:
+                            flags[1] += 1
+                            flags[2] = 1
+                        elif (pinky_tip.x - thumb_tip.x + .2) * 200 < 20 and flags[2] == 1:
+                            flags[1] += 1
+                            flags[2] = 0
+
+                        print(round((pinky_tip.x - thumb_tip.x + .2) * 200), "%")
+                        cv2.putText(
+                            flippedRGB, text="status: [ROTATING]", org=(10, 50), fontFace=cv2.FONT_HERSHEY_PLAIN,
+                            fontScale=3, color=(255, 255, 255), thickness=2
+                        )
                 else:
-                    pinky_tip = results.multi_hand_landmarks[0].landmark[20]
-                    thumb_tip = results.multi_hand_landmarks[0].landmark[4]
-                    sbc.set_brightness((pinky_tip.x - thumb_tip.x + .2) * 200)
-                    print(round((pinky_tip.x - thumb_tip.x + .2) * 200), "%")
-            else:
-                cv2.circle(flippedRGB, (int(x), int(y)), int(r), (0, 255, 0), 2)
-                if not prev_fist:
-                    sbc.fade_brightness(finish=1, start=100, blocking=False, increment=10)
-                    prev_fist = True
+                    cv2.putText(
+                        flippedRGB, text=f"Congrats! You've completed tutorial.", org=(10, flippedRGB.shape[0] - 40),
+                        fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1, color=(255, 255, 255), thickness=1
+                    )
+                    cv2.putText(
+                        flippedRGB, text=f"Run the script without `--help` flag", org=(10, flippedRGB.shape[0] - 10),
+                        fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1, color=(255, 255, 255), thickness=1
+                    )
 
-        res_image = cv2.cvtColor(flippedRGB, cv2.COLOR_RGB2BGR)
-        cv2.imshow("Res", res_image)
+            res_image = cv2.cvtColor(flippedRGB, code=cv2.COLOR_RGB2BGR)
+            cv2.imshow("Result", res_image)
+
+
+def main():
+    with mp.solutions.hands.Hands(
+            static_image_mode=True,
+            max_num_hands=1,
+            min_detection_confidence=.2
+    ) as handsDetector:
+        cap = cv2.VideoCapture(0)
+        prev_fist = False
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if cv2.waitKey(1) & 0xFF == ord("q") or not ret:
+                break
+            flipped = np.fliplr(frame)
+            flippedRGB = cv2.cvtColor(flipped, code=cv2.COLOR_BGR2RGB)
+            results: Results = handsDetector.process(flippedRGB)
+            cv2.putText(
+                flippedRGB, text="Specify --help flag to launch tutorial", org=(10, flippedRGB.shape[0] - 10),
+                fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1, color=(255, 255, 255), thickness=1
+            )
+            if results.multi_hand_landmarks is not None:
+                cv2.circle(
+                    flippedRGB,
+                    center=(int(results.multi_hand_landmarks[0].landmark[4].x * flippedRGB.shape[1]),
+                            int(results.multi_hand_landmarks[0].landmark[4].y * flippedRGB.shape[0])),
+                    radius=5, color=(255, 0, 0), thickness=-1
+                )
+                cv2.circle(
+                    flippedRGB, center=(int(results.multi_hand_landmarks[0].landmark[20].x * flippedRGB.shape[1]),
+                                        int(results.multi_hand_landmarks[0].landmark[20].y * flippedRGB.shape[0])),
+                    radius=5, color=(255, 0, 0), thickness=-1
+                )
+                cv2.line(
+                    flippedRGB,
+                    pt1=(int(results.multi_hand_landmarks[0].landmark[4].x * flippedRGB.shape[1]),
+                         int(results.multi_hand_landmarks[0].landmark[4].y * flippedRGB.shape[0])),
+                    pt2=(int(results.multi_hand_landmarks[0].landmark[20].x * flippedRGB.shape[1]),
+                         int(results.multi_hand_landmarks[0].landmark[20].y * flippedRGB.shape[0])),
+                    color=(255, 0, 0), thickness=2
+                )
+
+                (x, y), r = cv2.minEnclosingCircle(
+                    get_points(results.multi_hand_landmarks[0].landmark, flippedRGB.shape))
+                ws = palm_size(results.multi_hand_landmarks[0].landmark, flippedRGB.shape)
+                if 2 * r / ws > 1.6:
+                    cv2.circle(flippedRGB, center=(int(x), int(y)), radius=int(r), color=(0, 0, 255), thickness=2)
+                    if prev_fist:
+                        sbc.fade_brightness(finish=100, start=1, blocking=False, increment=10)
+                        prev_fist = False
+                        cv2.putText(
+                            flippedRGB, text="status: [PALM]", org=(10, 50), fontFace=cv2.FONT_HERSHEY_PLAIN,
+                            fontScale=3, color=(255, 255, 255), thickness=2
+                        )
+                    else:
+                        pinky_tip = results.multi_hand_landmarks[0].landmark[20]
+                        thumb_tip = results.multi_hand_landmarks[0].landmark[4]
+                        sbc.set_brightness((pinky_tip.x - thumb_tip.x + .2) * 200)
+                        print(round((pinky_tip.x - thumb_tip.x + .2) * 200), "%")
+                        cv2.putText(
+                            flippedRGB, text="status: [ROTATING]", org=(10, 50), fontFace=cv2.FONT_HERSHEY_PLAIN,
+                            fontScale=3, color=(255, 255, 255), thickness=2
+                        )
+                else:
+                    cv2.circle(flippedRGB, (int(x), int(y)), int(r), (0, 255, 0), 2)
+                    if not prev_fist:
+                        sbc.fade_brightness(finish=1, start=100, blocking=False, increment=10)
+                        prev_fist = True
+                    cv2.putText(
+                        flippedRGB, text="status: [FIST]", org=(10, 50), fontFace=cv2.FONT_HERSHEY_PLAIN,
+                        fontScale=3, color=(255, 255, 255), thickness=2
+                    )
+
+            res_image = cv2.cvtColor(flippedRGB, code=cv2.COLOR_RGB2BGR)
+            cv2.imshow("Result", res_image)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == "--help":
+        help_()
+    else:
+        main()
